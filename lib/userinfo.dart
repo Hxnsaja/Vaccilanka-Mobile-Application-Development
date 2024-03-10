609a39160e19eb40f73a1d8b9d98a468fc8f502f
Bhagya Gunaratne <148989832+BhagyaGunaratne@users.noreply.github.com>
2024-03-10 16:36:38 +0530

    Update userinfo.dart

--- lib/userinfo.dart
+++ lib/userinfo.dart
@@ -3,3 +3,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'User Information Form',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: UserInfoForm(),
+    );
+  }
+}
+
+class UserInfoForm extends StatefulWidget {
+  @override
+  _UserInfoFormState createState() => _UserInfoFormState();
+}
