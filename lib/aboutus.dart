4b16cb9230985bd16053188cdd8818f200000f84
Bhagya Gunaratne <148989832+BhagyaGunaratne@users.noreply.github.com>
2024-03-18 14:17:49 +0530

    Update aboutus.dart

--- lib/aboutus.dart
+++ lib/aboutus.dart
@@ -3,3 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'About Us Page',
