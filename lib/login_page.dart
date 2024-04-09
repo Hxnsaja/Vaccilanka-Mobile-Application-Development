8b724d8e795d6492d8b0f0e4faffbc7913c09205
LAtashini <148990352+LAtashini@users.noreply.github.com>
2024-04-10 00:37:28 +0530

    Add files via upload

---
+++ lib/login_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 196, 216, 228),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              "assets/Image1.jpg",
+              height: 100,
+              width: 100,
+            ),
+            const SizedBox(height: 20),
+//App name
+            const Text(
+              'VACCILANKA',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 24, 135, 226),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            //Uselogin text
+            const Text(
+              'User Login',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            //usename textbox
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'User Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            //password text box
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            //forgot password link
+            GestureDetector(
+              onTap: () {
+                //handle
+              },
+              child: const Text(
+                'Forgot Password?',
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 20),
+            //login button
+            ElevatedButton(
+              onPressed: () {
+                //handle
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 148, 186, 218),
+              ),
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
