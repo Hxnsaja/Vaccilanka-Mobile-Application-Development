0ad300e688f6c1539253b31e72086f4fe263e96b
Sandun Sahiru <134186922+sandunsahiru@users.noreply.github.com>
2024-04-10 09:24:17 +0530

    Settings Page

---
+++ lib/pages/settings.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:vaccilanka_mobile_application_development/pages/aboutus.dart';
+import 'package:vaccilanka_mobile_application_development/pages/contactus_page.dart';
+import 'package:vaccilanka_mobile_application_development/pages/userinfo.dart';
+import 'package:vaccilanka_mobile_application_development/pages/updates.dart';
+import 'package:vaccilanka_mobile_application_development/pages/home.dart';
+import 'package:vaccilanka_mobile_application_development/pages/widgets/bottomnav.dart';
+
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    final Color primaryColor = Color(0xFF00A1DE);
+    final Color secondaryColor = Color(0xFFF8F8F8);
+
+    return Scaffold(
+      backgroundColor: secondaryColor,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: primaryColor),
+          onPressed: () => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => HomePage())),
+        ),
+        title: Text('Settings', style: TextStyle(color: primaryColor)),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ListView(
+        children: [
+          Image.asset('assets/logo1.png', height: 100, width: 100, fit: BoxFit.contain), // Replace with your image asset
+          _buildSettingsItem('Account', () {/* Navigate to Account page */}),
+          _buildSettingsItem('Password Manager', () {/* Navigate to Password Manager page */}),
+          _buildSettingsItem('Notifications', () {/* Navigate to Notifications page */}),
+          _buildSettingsItem('Security and Privacy', () {/* Navigate to Security and Privacy page */}),
+          _buildSettingsItem('Theme', () {/* Navigate to Theme page */}),
+          _buildSettingsItem('Logout', () {/* Handle logout */}),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigation(selectedIndex: 3),
+    );
+  }
+
+  Widget _buildSettingsItem(String title, VoidCallback onTap) {
+    return ListTile(
+      title: Text(title),
+      trailing: Icon(Icons.arrow_forward_ios, color: Colors.black),
+      onTap: onTap,
+    );
+  }
+}
