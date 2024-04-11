6bfb0e7291967725ca425d8e6ef96a78a2749dad
Pulindu Hettiarachchi <pulinduhettiarachchi002@gmail.com>
2024-04-11 11:06:54 +0530

    Update userinfo.dart

--- lib/pages/userinfo.dart
+++ lib/pages/userinfo.dart
@@ -32,5 +32,24 @@ class _UserInfoFormState extends State<UserInfoForm> {
             color: Colors.black,
           ),
         ),
-        
-         
+      
+          backgroundColor : Color(0xFFEBF8F9),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+          onPressed: () => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => HomePage())),
+        ),
+ actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: 60, 
+              height: 80, 
+              child: Image.asset(
+                'assets/logo1.png',
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+          ),
+        ],
+      ),
