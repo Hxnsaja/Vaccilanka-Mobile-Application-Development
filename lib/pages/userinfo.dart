79d919045bbc307c40b0a8eb10fc14a893d558ee
Pulindu Hettiarachchi <pulinduhettiarachchi002@gmail.com>
2024-04-11 11:11:38 +0530

    userinfo.dart
    spacing and errors

--- lib/pages/userinfo.dart
+++ lib/pages/userinfo.dart
@@ -12,12 +12,12 @@ class UserInfoForm extends StatefulWidget {
 
 class _UserInfoFormState extends State<UserInfoForm> {
   final _formKey = GlobalKey<FormState>();
-  final UserInfoController _userInfoController = UserInfoController(); 
+  final UserInfoController _userInfoController = UserInfoController();
 
- @override
+  @override
   void initState() {
     super.initState();
-    _userInfoController.loadUserData(); 
+    _userInfoController.loadUserData();
   }
 
   @override
@@ -32,19 +32,19 @@ class _UserInfoFormState extends State<UserInfoForm> {
             color: Colors.black,
           ),
         ),
-      
-          backgroundColor : Color(0xFFEBF8F9),
+        backgroundColor: Color(0xFFEBF8F9),
         centerTitle: true,
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios, color: Colors.black),
-          onPressed: () => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => HomePage())),
+          onPressed: () => Navigator.of(context)
+              .pushReplacement(MaterialPageRoute(builder: (_) => HomePage())),
         ),
- actions: [
+        actions: [
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: SizedBox(
-              width: 60, 
-              height: 80, 
+              width: 60,
+              height: 80,
               child: Image.asset(
                 'assets/logo1.png',
                 fit: BoxFit.fitHeight,
@@ -53,30 +53,37 @@ class _UserInfoFormState extends State<UserInfoForm> {
           ),
         ],
       ),
-
-    body: SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(16.0),
           child: Form(
             key: _formKey,
             child: Column(
               children: <Widget>[
-                buildFieldWithTitle('Child ID', _userInfoController.childIdController),
-                buildFieldWithTitle('First Name', _userInfoController.firstNameController),
-                buildFieldWithTitle('Last Name', _userInfoController.lastNameController),
-                buildFieldWithTitle('Date of Birth', _userInfoController.dobController),
-                buildFieldWithTitle('Guardian Name', _userInfoController.guardianNameController),
-                buildFieldWithTitle('Address', _userInfoController.guardianAddressController),
-                buildFieldWithTitle('Gender', _userInfoController.genderController),
-                buildFieldWithTitle('Contact Number', _userInfoController.phoneNumberController),
-                buildFieldWithTitle('Email', _userInfoController.emailController),
+                buildFieldWithTitle(
+                    'Child ID', _userInfoController.childIdController),
+                buildFieldWithTitle(
+                    'First Name', _userInfoController.firstNameController),
+                buildFieldWithTitle(
+                    'Last Name', _userInfoController.lastNameController),
+                buildFieldWithTitle(
+                    'Date of Birth', _userInfoController.dobController),
+                buildFieldWithTitle('Guardian Name',
+                    _userInfoController.guardianNameController),
+                buildFieldWithTitle(
+                    'Address', _userInfoController.guardianAddressController),
+                buildFieldWithTitle(
+                    'Gender', _userInfoController.genderController),
+                buildFieldWithTitle('Contact Number',
+                    _userInfoController.phoneNumberController),
+                buildFieldWithTitle(
+                    'Email', _userInfoController.emailController),
                 SizedBox(height: 20),
                 Center(
                   child: ElevatedButton(
                     onPressed: () {
                       if (_formKey.currentState!.validate()) {
-
-                      _userInfoController.updateUserInformation(); 
+                        _userInfoController.updateUserInformation();
                       }
                     },
                     style: ElevatedButton.styleFrom(
