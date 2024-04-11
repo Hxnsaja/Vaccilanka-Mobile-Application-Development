a197b6e27278742b091c667fb051af7952c111c7
Pulindu Hettiarachchi <pulinduhettiarachchi002@gmail.com>
2024-04-11 11:24:25 +0530

    Update formwidget.dart

--- lib/pages/widgets/form_widgets.dart
+++ lib/pages/widgets/form_widgets.dart
@@ -16,3 +16,62 @@ Widget buildFieldWithTitle(String title, TextEditingController controller) {
           ),
         ),
       ),
+Padding(
+        padding: EdgeInsets.only(bottom: 8.0),
+        child: TextFormField(
+          controller: controller,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: BorderSide(
+                color: Colors.blue,
+                width: 1.0,
+              ),
+            ),
+ focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: BorderSide(
+                color: Colors.blue,
+                width: 1.0,
+              ),
+            ),
+            hintText: 'Enter $title',
+            hintStyle: TextStyle(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w600,
+              color: Colors.grey[400],
+            ),
+
+            contentPadding:
+                EdgeInsets.symmetric(vertical: 10.0, horizontal: 12.0),
+          ),
+          validator: (value) {
+            if (value!.isEmpty) {
+              return 'Please enter $title';
+            }
+            return null;
+          },
+        ),
+      ),
+    ],
+  );
+}  
+Widget buildShortFieldWithTitle(String title, TextEditingController controller, VoidCallback onChangePressed) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Padding(
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w600,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ),
+
+      
